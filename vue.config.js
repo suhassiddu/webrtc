5cca4d9498fcaca7749dd6abfd20de09970d8280
suhas siddarama <suhas.siddarama@signzy.com>
2020-01-03 18:06:55 +0530

    base

--- vue.config.js
+++ vue.config.js
@@ -4,3 +4,11 @@ module.exports = {
 
 // "node-sass": "^4.13.0",
 // "sass-loader": "^7.0.1",
+
+/*
+"serve": "vue-cli-service serve",
+    "run:server": "node ./server/index.js",
+    "debug:server": "node --inspect=0.0.0.0:9229 ./server/index.js",
+    "build": "vue-cli-service build",
+    "lint": "vue-cli-service lint"
+*/
